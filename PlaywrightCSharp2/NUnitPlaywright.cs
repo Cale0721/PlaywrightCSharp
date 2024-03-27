4c7c3cd18103fb2caa9b29d9f85475ecee61cf89
Caleb Moore <Cale0721@gmail.com>
2024-03-26 21:53:25 -0400

    updated to video six

--- PlaywrightCSharp2/NUnitPlaywright.cs
+++ PlaywrightCSharp2/NUnitPlaywright.cs
@@ -16,10 +16,14 @@ public class NUnitPlaywright : PageTest
     public async Task Test1()
     {
         
+        var lnkLogin = Page.Locator("text=Login");
         await Page.ClickAsync("text=Login");
         await Page.FillAsync("#UserName", "admin");
         await Page.FillAsync("#Password", "password");
-        await Page.ClickAsync("text=Log in");
+
+        var btnLogin = Page.Locator("input", new PageLocatorOptions { HasTextString = "Log in" });
+        await btnLogin.ClickAsync();
+        //await Page.ClickAsync("text=Log in");
         await Expect(Page.Locator("text='Employee Details'")).ToBeVisibleAsync();
 
 
--- PlaywrightCSharp2/UnitTest1.cs
+++ PlaywrightCSharp2/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.Playwright;
+using PlaywrightCSharp2.Pages;
 using static NUnit.Framework.Assert;
 
 
@@ -11,6 +12,27 @@ public class Tests
     {
     }
 
+    [Test]
+    public async Task TestWithPOM()
+    {
+        //Playwright
+        using var playwright = await Playwright.CreateAsync();
+        //Browser
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+        //Page
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("http://www.eaapp.somee.com");
+
+        LoginPage loginPage = new LoginPage(page);
+        await loginPage.ClickLogin();
+        await loginPage.Login("admin", "password");
+        var isExist = await loginPage.IsEmployeeDetailsExists();
+        Assert.IsTrue(isExist);
+    }
+    
     [Test]
     public async Task Test1()
     {
@@ -26,13 +48,13 @@ public class Tests
         await page.GotoAsync("http://www.eaapp.somee.com");
         await page.ClickAsync("text=Login");
         await page.ScreenshotAsync(new PageScreenshotOptions
-            {
-                Path = "test.png"
-            });
+        {
+            Path = "test.png"
+        });
         await page.FillAsync("#UserName", "admin");
         await page.FillAsync("#Password", "password");
         await page.ClickAsync("text=Log in");
         var isExist = await page.Locator("text='Employee Details'").IsVisibleAsync();
-        Assert.IsTrue(isExist);
+        //Assert.IsTrue(isExist);
     }
 }
